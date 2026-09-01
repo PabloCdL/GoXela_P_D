b6a42ea270a8a2bd2256a834403f9135c6b10898
PabloCdL <angelcoyoy07@gmail.com>
2026-08-31 21:10:46 -0600

    Se creo la clase, Motocicleta, bicicleta, paquete

---
+++ Bicicleta.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoXela_P__D
+{
+    internal class Bicicleta : Vehiculos
+    {
+        private double precio;
+
+        public double Precio
+        {
+            get { return precio; }
+            set { precio = value; } // ya definido
+        }
+
+        public Bicicleta(string codigo, int capacidadMaxima, string marca, string modelo, double costo, Estado estado, double precio) : base(codigo, capacidadMaxima, marca, modelo, estado)
+        {
+
+        }
+    }
+}
---
+++ Paquete.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoXela_P__D
+{
+    public enum EstadoPaquete
+    {
+        //estados
+    }
+    internal class Paquete
+    {
+        private string codigo;
+
+        public string Codigo
+        {
+            get { return codigo; }
+            set { codigo = value; }
+        }
+
+        private string descripcion;
+
+        public string Descripcion
+        {
+            get { return descripcion; }
+            set { descripcion = value; }
+        }
+
+        private double peso;
+
+        public double Peso
+        {
+            get { return peso; }
+            set { peso = value; }
+        }
+
+        private string direccionOri;
+
+        public string DireccionOri
+        {
+            get { return direccionOri; }
+            set { direccionOri = value; }
+        }
+
+        private string direccionDes;
+
+        public string DireccionDes
+        {
+            get { return direccionDes; }
+            set { direccionDes = value; }
+        }
+
+        private EstadoPaquete estado;
+
+        public EstadoPaquete Estado
+        {
+            get { return estado; }
+            set { estado = value; }
+        }
+
+        public Paquete(string codigo, string descripcion, double peso, string direccionOri, string direccionDes, EstadoPaquete estado)
+        {
+            Codigo = codigo;
+            Descripcion = descripcion;
+            Peso = peso;
+            DireccionOri = direccionOri;
+            DireccionDes = direccionDes;
+            Estado = estado;
+        }
+    }
+}
