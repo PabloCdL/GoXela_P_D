30ef584c6dd51bebb8a998c4acf94c8f9f0b6f78
PabloCdL <angelcoyoy07@gmail.com>
2026-08-31 20:57:48 -0600

    Se creo la clase clientes

---
+++ Clientes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoXela_P__D
+{
+    internal class Clientes : Persona
+    {
+        private int cantidadSolicitudes;
+
+        public int CantidadSolicitudes
+        {
+            get { return cantidadSolicitudes; }
+            set { cantidadSolicitudes = value; }
+        }
+
+        public Clientes(string codigo, string nombreCompleto, int numeroTelefonico, string direccion, string correo, int cantidadSolicitudes) : base(codigo, nombreCompleto, numeroTelefonico, direccion, correo)
+        {
+            CantidadSolicitudes = cantidadSolicitudes;
+        }
+    }
+}
