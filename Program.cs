0fecf74166121ffd6ec36e45a9938227f3080400
PabloCdL <angelcoyoy07@gmail.com>
2026-09-01 17:38:31 -0600

    Creacion del Menú principal

--- Program.cs
+++ Program.cs
@@ -10,7 +10,60 @@ namespace GoXela_P__D
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Prueba de rama-diana");
+            int op;
+            do
+            {            
+                Console.WriteLine($"=========================");
+                Console.WriteLine($"      GOXELA DELIVERY    ");
+                Console.WriteLine($"=========================");
+                Console.WriteLine($"1. Gestión de clientes");
+                Console.WriteLine($"2. Gestión de repartidores");
+                Console.WriteLine($"3. Gestión de vehículos");
+                Console.WriteLine($"4. Gestión de paquetes");
+                Console.WriteLine($"5. Gestión de entregas");
+                Console.WriteLine($"6. Gestión de incidencias");
+                Console.WriteLine($"7. Reportes");
+                Console.WriteLine($"8. Salir");
+
+                if(!int.TryParse(Console.ReadLine(), out op))
+                {
+                    Console.WriteLine($"Dato invalido... Intente de nuevo");
+                }
+                else
+                {
+                    switch (op)
+                    {
+                        case 1:
+                            //Clientes
+                            break;
+                        case 2:
+                            //Repartidores
+                            break;
+                        case 3:
+                            //Vehículos
+                            break;
+                        case 4:
+                            //Paquetes
+                            break;
+                        case 5:
+                            //Entregas 
+                            break;
+                        case 6:
+                            //Incidencias
+                            break;
+                        case 7: 
+                            //Reportes
+                            break;
+                        case 8:
+                            //Salir
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                
+            }while(op != 8);
+
         }
     }
 }
